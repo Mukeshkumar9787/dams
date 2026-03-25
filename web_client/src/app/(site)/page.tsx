134f00465de0f0507a2996d7c420e7f8e45b252e
Mukeshkumar <mukeshkumar87086@gmail.com>
2026-03-25 15:45:41 +0530

    dynamic generation

--- web_client/src/app/(site)/page.tsx
+++ web_client/src/app/(site)/page.tsx
@@ -2,6 +2,8 @@ import Home from "@/components/Home";
 import { PROJECT_DETAILS } from "@/utils/constants";
 import { Metadata } from "next";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: PROJECT_DETAILS.name,
   description: PROJECT_DETAILS.description,
